38408e3fb2c0679400dbbc504bc8a28086d638bb
Matt Cascardi <mattcascardi@gmail.com>
2023-03-16 06:52:58 +0000

    Implemented API to proxy requests to httpbin.org

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,57 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Add Http client to use for making requests
+use Illuminate\Support\Facades\Http;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/proxy/{method}', function($method = 'anything', Request $request) {
+
+    $allowedMethods = [
+        'anything', 'get', 'headers', 'ip', 'user-agent', 'cache', 'etag',
+        'response-headers', 'html', 'json', 'robots.txt', 'xml', 'uuid', 'image'
+    ];
+
+    if (!in_array($method, $allowedMethods)) {
+        return response("The {$method} method is not allowed", 405);
+    }
+
+    $reqParams = $request->all();
+
+    // Send the request parameters along to the upstream server
+    $response  = Http::get("http://httpbin.org/{$method}", $reqParams);
+
+    // Return JSON
+    $body = (($response->header('Content-Type') == 'application/json') ? $response->json() : $response->body());
+
+    return [
+        'params' => $reqParams,
+        'method' => $request->method(),
+        'headers' => $response->headers(),
+        'response' => $body
+    ];
+});
+
+
+Route::post('/proxy/{method}', function($method = 'POST', Request $request){
+    $reqParams = $request->all();
+
+    $response = Http::post("http://httpbin.org/{$method}", $reqParams);
+
+    // Return JSON
+    $body = (($response->header('Content-Type') == 'application/json') ? $response->json() : $response->body());
+
+    return [
+        'params' => $reqParams,
+        'method' => $request->method(),
+        'headers' => $response->headers(),
+        'response' => $body
+    ];
+});
+
+Route::get('/proxy', function (Request $request) {
+    return redirect('/api/proxy/anything');
+});
+
+Route::post('/proxy', function (Request $request) {
+    return redirect('/api/proxy/anything');
 });
